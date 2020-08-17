f434a68407542ea632a47dbc807ec63c9fb02546
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-08-17 12:51:06 -0300

    fix: função de autenticação. add dependencia de token organizando rotas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,41 +1,76 @@
 <?php
 
 namespace App\Models;
+
 namespace App\Http\Controllers;
 
 use App\Models\ClienteModel;
 use App\User;
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-   /**
+    /**
      * Create a new AuthController instance.
      *
      * @return void
      */
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth:api', ['except' => ['login']]);
     }
 
-      /**
+    /**
      * Get a JWT via given credentials.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request){
-        
-        $request-> only('email', 'senha');
+    public function login(Request $request)
+    {
 
-        if (!$token = Auth::attempt($request)) {
-            return response()->json(['message'=> 'Não autorizado' ], 401);
+        $credentials = $request->only('email', 'senha');
+
+
+        // Find the user by email
+        $cliente = ClienteModel::where('email', $credentials['email'])->first();
+
+        if (!$cliente) {
+            // You wil probably have some sort of helpers or whatever
+            // to make sure that you have the same response format for
+            // differents kind of responses. But let's return the
+            // below respose for now.
+            return response()->json([
+                'error' => 'Email does not exist.'
+            ], 400);
         }
 
-        return $this-> respondWithToken($token);
+        // Verify the password and generate the token
+        if (Hash::check($credentials['senha'], $cliente->senha)) {
+            return response()->json([
+                'token' => $this->jwt($cliente)
+            ], 200);
+        }
+
+        return response()->json(['message' => 'Falha ao realizar login'], 401);
     }
 
-     /**
+    protected function jwt(ClienteModel $cliente) {
+        $payload = [
+            'iss' => "lumen-jwt", // Issuer of the token
+            'sub' => $cliente->id, // Subject of the token
+            'iat' => time(), // Time when JWT was issued.
+            'exp' => time() + 60*60 // Expiration time
+        ];
+
+        // As you can see we are passing `JWT_SECRET` as the second parameter that will
+        // be used to decode the token in the future.
+        return JWT::encode($payload, env('JWT_SECRET'));
+    }
+
+    /**
      * Get the token array structure.
      *
      * @param  string $token
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
-    $router->group(['prefix' => 'api'], function () use ($router) {
-        $router->group(['prefix' => 'cliente'], function () use ($router) {
-            $router->get('/', 'CadastroClienteController@getAll');
-            $router->post('/', 'CadastroClienteController@store');
-            $router->post('/', 'AuthController@login');
-            $router->post('/', 'CadastroEmpresaController@store');
-        });
+/**
+ * @var \Laravel\Lumen\Routing\Router $router
+ */
+
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'cliente'], function () use ($router) {
+        $router->get('/', 'CadastroClienteController@getAll');
+        $router->post('/', 'CadastroClienteController@store');
     });
+    $router->group(['prefix' => 'empresa'], function () use ($router) {
+        $router->get('/', 'CadastroEmpresaController@store');
+    });
+    $router->post('login', 'AuthController@login');
+});
